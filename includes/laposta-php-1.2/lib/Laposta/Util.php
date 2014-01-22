b23111ed810bb03701e054f131f484fd96a25f29
Stijn van der Ree <stijn@laposta.nl>
2014-01-22 09:17:48 -0500

    Fill repro

---
+++ includes/laposta-php-1.2/lib/Laposta/Util.php
@@ -0,0 +1,57 @@
+<?php
+class Laposta_Util {
+
+	public static function connect($url, $headers, $api_key, $post = null, $method = null, $timeout = 15) {
+
+		$error = false;
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HEADER, false);
+		curl_setopt($ch, CURLOPT_ENCODING, 'UTF-8');
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+		curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+		curl_setopt($ch, CURLOPT_FORBID_REUSE, true);
+		curl_setopt($ch, CURLOPT_FRESH_CONNECT, true); 
+		curl_setopt($ch, CURLOPT_USERPWD, $api_key . ':');
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+		if ($post) {
+			curl_setopt($ch, CURLOPT_POST, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+		}
+
+		if ($method == 'DELETE') {
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');  
+		}
+
+		$body = curl_exec($ch);
+		$info = curl_getinfo($ch);
+
+		// controleer op fouten
+		$error = false;
+		if (curl_errno($ch)) {
+			$error = true;
+			$error_msg = curl_error($ch);
+		}
+		curl_close($ch);
+
+		return array('error' => $error, 'error_msg' => $error_msg, 'status' => $info['http_code'], 'body' => $body, 'info' => $info);
+	}
+
+	public static function utf8($value) {
+
+		if (is_string($value)) return utf8_encode($value);
+		else if (is_array($value)) {
+			function encode_items(&$item, $key) {
+				$item = utf8_encode($item);
+			}
+			array_walk_recursive($value, 'encode_items');
+		}
+
+		return $value;
+	}
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== Laposta ===
+Contributors: stijnvanderree
+Tags: laposta, nieuwsbrieven, aanmelden
+Requires at least: 3.0
+Tested up to: 3.8
+Stable tag: 0.4
+License: GPLv2 or later
+
+This plugin can be used to easily place a widget with a subscribe form for the
+Laposta newsletter software (http://www.laposta.nl).
+
+== Description ==
+
+Laposta is a Dutch emailmarketing solution. Use this plugin to add a
+subscribeform to your WordPress blog.
+
+== Installation ==
+
+Unzip the file in the plugins directory, and activate the plugin in the
+Plugins screen. Then go to the Settings to setup the connection to Laposta.
+
+== Changelog ==
+
+= 0.4 =
+
+* Added extensive error reporting
+
+= 0.3 =
+
+* Added notifications in widget
+* Fixed small layout issues
+
+= 0.2 =
+
+* Updated text 
+* Fixed small coding issues
+
+= 0.1 =
+
+* First version. Bugs expected. Mail stijn@laposta.nl to get them fixed.
